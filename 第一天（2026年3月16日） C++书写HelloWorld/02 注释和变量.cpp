5a0ed097e3166e732f4ddd8167bd62be2d8003ee
惊鸿 <1178121989@qq.com>
2026-03-16 22:44:52 +0800

    注释和变量

---
+++ 第一天（2026年3月16日） C++书写HelloWorld/02 注释和变量.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	//这是单行注释
+//	int age = 21;				//整型
+//	double height = 175.5;		//双精度浮点型
+//	char gender = 'M';			//字符型
+//
+//	cout << "age: " << age << endl;
+//	cout << "height: " << height << " cm" << endl;
+//	cout << "gender: " << gender << endl;
+//
+//	/*
+//	这是多行注释
+//	可以写多行说明
+//	*/
+//
+//	system("pause");
+//
+//	return 0;
+//}
